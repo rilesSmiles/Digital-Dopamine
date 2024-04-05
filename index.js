fd879bce723d9499df1acf8e792912bab2798aec
rilesSmiles <riley.schmitz@edu.sait.ca>
2024-04-05 15:56:25 -0600

    media queries complete

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-'use strict';
-var parent = require('../stable/get-iterator');
+console.log('i am alive');
+//var parent = require('../stable/get-iterator');
 
-module.exports = parent;
+//module.exports = parent;
 
-function getnews(){
-    fetch('https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?category=graphicdesign&apikey=e7fbaba830f74892aea41dcc725c0450',{headers: new Headers({"X-Requested-With":"weugfiehvkjdcn"})})
+async function getnews(){
+    const apiKey = 'e7fbaba830f74892aea41dcc725c0450';
+    const apiUrl = `https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?category=graphicdesign&apikey=${apiKey}`
+
+    fetch(apiUrl,{headers: new Headers({"X-Requested-With":"weugfiehvkjdcn"})})
     .then(a=>a.json())
     .then(response=>{
       for(var i=0; i<response.articles.length;i++){
         document.getElementById("output").innerHTML +=
-        "<div style='padding-top:20px;'><img style='float:left; width: 150px;'src='"+response.articles[i].urlToImage+'"><h1>"+response.articles[i].title+"</h1>"+response.artices[i].source.name+"<br>"+response.artices[i].description+" <a href=';""+response.artices[i].url+";'target='_any;'>"+response.artices[i].url+";</a></div>";
+        `<div style='padding-top:20px;'><img style='float:left; width: 150px;'src="${response.articles[i].urlToImage}"><h1>${response.articles[i].title}</h1>${response.artices[i].source.name}<br>${response.artices[i].description} <a href=';"${response.artices[i].url};'target='_any;'>${response.artices[i].url};</a></div>`;
       }
     })
   }
+
